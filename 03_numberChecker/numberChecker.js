94086dfcbbd13ebb544d31efd032fdd76a2f6376
silverdan007 <danielofuani4@gmail.com>
2025-05-16 01:09:01 +0100

    completed tasks helloWorld addNumbers numberChecker mathEquations joinStrings

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,12 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+// Modify the code so it returns `true` 
+// when the number is greater than or equal to 10, and `false` if it is less than 10.
 
 // Do not edit below this line
 module.exports = numberChecker;
